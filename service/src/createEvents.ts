dbbb41cc5efb2943c1ae65b0c08b65435002b5e1
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2022-10-21 08:33:32 -0500

    rxfx/service@1.0.2 create{,Process,Collection}Events helpers

---
+++ service/src/createEvents.ts
@@ -0,0 +1,50 @@
+import { actionCreatorFactory } from 'typescript-fsa';
+import { ProcessEvents, CollectionEvents } from './types';
+/**
+ *
+ * @param type - the action/event/subevent
+ * @returns an ActionCreator whose match property is a TS type guard
+ * @see https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
+ */
+export function createEvent<T>(type: string) {
+  const [root, ...paths] = type.split('/');
+  const isMultipart = paths.length > 0;
+  const ns = isMultipart ? root : '';
+  const rest = isMultipart ? paths.join('/') : root;
+
+  const actionAF = actionCreatorFactory(ns);
+  return actionAF<T>(rest);
+}
+
+export function createProcessEvents<TRequest, TNext, TError>(
+  ns: string
+): ProcessEvents<TRequest, TNext, TError> {
+  return {
+    request: createEvent<TRequest>(`${ns}/request`),
+    cancel: createEvent<void>(`${ns}/cancel`),
+    started: createEvent<void>(`${ns}/started`),
+    next: createEvent<TNext>(`${ns}/next`),
+    error: createEvent<TError>(`${ns}/error`),
+    complete: createEvent<void>(`${ns}/complete`),
+    canceled: createEvent<void>(`${ns}/canceled`),
+  };
+}
+
+export function createCollectionEvents<
+  TFetcher = void,
+  TRecord = any,
+  TResponse extends TRecord = TRecord,
+  TError = Error
+>(ns: string): CollectionEvents<TFetcher, TRecord, TResponse, TError> {
+  return {
+    load: createProcessEvents<TFetcher, TRecord, TError>(`${ns}/load`),
+    refresh: createProcessEvents<TFetcher, TRecord, TError>(`${ns}/refresh`),
+    post: createProcessEvents<TRecord, TResponse, TError>(`${ns}/post`),
+    update: createProcessEvents<Partial<TRecord>, TResponse, TError>(
+      `${ns}/update`
+    ),
+    delete: createProcessEvents<Partial<TRecord>, TResponse, TError>(
+      `${ns}/delete`
+    ),
+  };
+}
---
+++ service/src/types.ts
@@ -0,0 +1,34 @@
+import { ActionCreator } from 'typescript-fsa';
+
+/** The events a process emits or responds to throughout its lifecycle. */
+export interface ProcessEvents<TRequest, TNext, TError> {
+  /** invokes the service */
+  request: ActionCreator<TRequest>;
+  /** cancels the current invocation of the service */
+  cancel: ActionCreator<void>;
+  started: ActionCreator<void>;
+  next: ActionCreator<TNext>;
+  error: ActionCreator<TError>;
+  complete: ActionCreator<void>;
+  canceled: ActionCreator<void>;
+}
+
+/** The processes and corresponding events a data collection emits or responds to throughout its lifecycle. */
+
+export interface CollectionEvents<
+  TFetcher = void,
+  TRecord = any,
+  TResponse extends TRecord = TRecord,
+  TError = Error
+> {
+  load: ProcessEvents<TFetcher, TRecord, TError>;
+  refresh: ProcessEvents<TFetcher, TRecord, TError>;
+  post: ProcessEvents<TRecord, TResponse, TError>;
+  update: ProcessEvents<Partial<TRecord>, TResponse, TError>;
+  delete: ProcessEvents<Partial<TRecord>, TResponse, TError>;
+}
+
+/** Helpful, optional interface to differentiate service actions. */
+export interface HasSubtype<T = string> {
+  subtype: T;
+}
