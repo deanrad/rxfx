2ab044e80e8404dd076b96709d717da83b3dfec5
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2025-06-30 12:43:54 -0500

    rxfx@0.2.0

--- all/src/index.ts
+++ all/src/index.ts
@@ -1,9 +1,11 @@
 export * from './after';
 export * from './bus';
 export * from './fsa';
+export * from './operators';
 export * from './perception';
 export * from './service';
 export * from './effect';
 
 export { Subscription, ObservableInput, interval, from } from 'rxjs';
+export { concatMap, switchMap, mergeMap, exhaustMap } from 'rxjs/operators';
 export * from 'rxjs/ajax';
